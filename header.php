f92b3bd86eec6f4dfda468358a72e5647268536c
ahmetdereli <aries@Ahmet-MacBook-Pro.local>
2014-07-19 00:23:38 +0300

    Change site title method

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title><?php wp_title(''); ?></title>
+    <title><?php bloginfo( 'name' ); ?></title>
     <meta name="description" content="">
 
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
