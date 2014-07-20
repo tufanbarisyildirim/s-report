77970331c26a765106d45bde36462b70154eafc9
Adem ilter <ademilter@gmail.com>
2014-07-20 17:56:24 +0300

    add infinite-scroll (thx @mkdgr)

--- functions.php
+++ functions.php
@@ -1,10 +1,32 @@
 <?php
 
-// TRANSLATION
+
+// --------------------------------------------------------
+// Auto delete first "Hello Word" post
+
+$autodeletefirst = $wpdb->get_results(
+    "
+  SELECT *
+  FROM $wpdb->posts
+  WHERE ID = 1
+  "
+);
+if ($autodeletefirst[0]->post_status == "publish") {
+    wp_delete_post(1, false);
+}
+
+// --------------------------------------------------------
+// Translation
+
 load_theme_textdomain('s-report', get_template_directory() . '/languages');
 
 
-function tokenText($str) {
+// --------------------------------------------------------
+// Translation
+
+
+function tokenText($str)
+{
     // thx fatih (github.com/f)
     $matches = preg_split("/\;\s*\\$/uism", $str);
     $object = array();
@@ -14,3 +36,37 @@ function tokenText($str) {
     }
     return $object;
 }
+
+// --------------------------------------------------------
+// Infinite scroll
+
+function scripts()
+{
+    wp_enqueue_script('infinite_scrolling', get_stylesheet_directory_uri() . '/js/jquery.infinitescroll.min.js', array('jquery'));
+}
+
+function set_infinite_scrolling()
+{
+    if (is_home()) {
+        ?>
+        <script type="text/javascript">
+            var inf_scrolling = {
+                loading: {
+                    img: "<?php echo get_template_directory_uri(); ?>/img/loading-bubbles.svg", // https://github.com/jxnblk/loading
+                    msgText: "<?php _e('Loading new posts', 's-report'); ?>",
+                    finishedMsg: "All post loaded!",
+                },
+                "nextSelector": ".nav a:last-child",
+                "navSelector": ".nav",
+                "itemSelector": ".Event",
+                "contentSelector": ".Events"
+            };
+
+            jQuery(inf_scrolling.contentSelector).infinitescroll(inf_scrolling);
+        </script>
+    <?php
+    }
+}
+
+add_action('wp_footer', 'set_infinite_scrolling', 100);
+add_action('wp_enqueue_scripts', 'scripts');
--- index.php
+++ index.php
@@ -1,77 +1,79 @@
 <?php get_header(); ?>
 
 
-<div class="Page">
-    <div class="Events">
+    <div class="Page">
+        <div class="Events">
 
 
-        <?php
+            <?php
 
 
-        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-        $args = array('paged' => $paged);
-        query_posts($args);
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            $args = array('paged' => $paged);
+            query_posts($args);
 
 
-        if (have_posts()) : while (have_posts()) : the_post();
+            if (have_posts()) : while (have_posts()) : the_post();
 
 
-            // Post cat name
-            $Cat = get_the_category();
-            $Domain = $Cat[0]->slug;
+                // Post cat name
+                $Cat = get_the_category();
+                $Domain = $Cat[0]->slug;
 
 
-            // Post tag
-            $PostTags = get_the_tags();
-            unset($Action);
-            $Action = array();
+                // Post tag
+                $PostTags = get_the_tags();
+                unset($Action);
+                $Action = array();
 
-            if ($PostTags) {
-                foreach ($PostTags as $tag) {
-                    array_push($Action, $tag->name);
+                if ($PostTags) {
+                    foreach ($PostTags as $tag) {
+                        array_push($Action, $tag->name);
+                    }
                 }
-            }
 
 
-            // domain icon size
-            include "icon-options.php";
+                // domain icon size
+                include "icon-options.php";
 
-            $widthDomainIcon = $DomainOption[$Domain]['sizeWeightIcon'];
-            $heightDomainIcon = $DomainOption[$Domain]['sizeHeightIcon'];
-
-
-            // Content
-            $Data = tokenText(get_the_content());
+                $widthDomainIcon = $DomainOption[$Domain]['sizeWeightIcon'];
+                $heightDomainIcon = $DomainOption[$Domain]['sizeHeightIcon'];
 
 
-            // Event
-            include "event/instagram.php";
-            include "event/facebook.php";
-            include "event/foursquare.php";
-            include "event/twitter.php";
-            include "event/soundcloud.php";
-            include "event/vimeo.php";
-            include "event/tumblr.php";
-            include "event/youtube.php";
-            include "event/dribbble.php";
-            include "event/imdb.php";
-            include "event/instapaper.php";
-            include "event/etsy.php";
-            include "event/github.php";
-            include "event/iphone.php";
-            include "event/calendar.php";
+                // Content
+                $Data = tokenText(get_the_content());
 
 
-        endwhile;
+                // Event
+                include "event/instagram.php";
+                include "event/facebook.php";
+                include "event/foursquare.php";
+                include "event/twitter.php";
+                include "event/soundcloud.php";
+                include "event/vimeo.php";
+                include "event/tumblr.php";
+                include "event/youtube.php";
+                include "event/dribbble.php";
+                include "event/imdb.php";
+                include "event/instapaper.php";
+                include "event/etsy.php";
+                include "event/github.php";
+                include "event/iphone.php";
+                include "event/calendar.php";
 
-            next_posts_link(__('Older posts', 's-report'));
-            previous_posts_link(__('Newer posts', 's-report'));
 
-        endif; ?>
+            endwhile; ?>
+
+                <div class="nav">
+                    <?php next_posts_link(__('Older posts', 's-report')); ?>
+                    <?php previous_posts_link(__('Newer posts', 's-report')); ?>
+                </div>
+
+            <?php endif; ?>
 
 
+        </div>
     </div>
-</div>
 
 
 <?php get_footer(); ?>
